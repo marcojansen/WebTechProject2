0634dddbeb2f88a3d262eed2075124cf0062f6c7
Daan <daanveldhof@hotmail.com>
2014-10-01 20:02:04 +0200

    Contactgegevens mooier gemaakt

--- javascript/script.js
+++ javascript/script.js
@@ -5,33 +5,40 @@ var timer;
 
 function onInit() {
     imageInit();
-    timer = setInterval(function () {nextImage();}, 3000);
+    timer = setInterval(function() {
+        nextImage();
+    }, 3000);
 }
 
 function imageInit() {
     bigImage = document.getElementById("largeImage");
-    
-    for(i = 1; i < 5; i++ ){
+
+    for (i = 1; i < 5; i++) {
         images.push(document.getElementById("image" + i));
-    }        
-    images[0].onclick = function () {setImage(0);};
-    images[1].onclick = function () {setImage(1);};
-    images[2].onclick = function () {setImage(2);};
-    images[3].onclick = function () {setImage(3);};
+    }
+    images[0].onclick = function() {
+        setImage(0);
+    };
+    images[1].onclick = function() {
+        setImage(1);
+    };
+    images[2].onclick = function() {
+        setImage(2);
+    };
+    images[3].onclick = function() {
+        setImage(3);
+    };
 }
 
-function setImage(index){
+function setImage(index) {
     bigImage.src = images[index].src;
     clearInterval(timer);
-    timer = setInterval(function () {nextImage();}, 3000);
+    timer = setInterval(function() {
+        nextImage();
+    }, 3000);
 }
 
 function nextImage() {
     bigImage.src = images[imageIndex].src;
     imageIndex = (imageIndex + 1) % images.length;
 }
-
-
-
-
-
